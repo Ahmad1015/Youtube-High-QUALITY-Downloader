b18abba460d274ac4f7064cc4e1a3a647b1f9658
Ahmad <mahmedraza90@gmail.com>
2025-01-27 07:21:02 +0000

    Working

---
+++ Downloader.py
@@ -0,0 +1,25 @@
+import yt_dlp
+
+def download_youtube_videos(video_urls):
+    try:
+        ydl_opts = {
+            'format': 'bestvideo+bestaudio/best',  # Select best quality
+            'outtmpl': '%(title)s.%(ext)s',       # Save with video title as filename
+        }
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            for url in video_urls:
+                print(f"Downloading video from URL: {url}")
+                ydl.download([url])
+        print("All downloads completed.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+if _name_ == "_main_":
+    
+    video_urls = [
+        "Link 1",
+        "Link 2",
+    ]
+    
+  
+    download_youtube_videos(video_urls)
